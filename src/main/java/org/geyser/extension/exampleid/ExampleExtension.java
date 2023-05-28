47614575a69bddecb241676215f3c9f9113db304
onebeastchris <reichel32574@gmail.com>
2023-05-28 13:30:14 +0200

    Update ExampleExtension.java

--- src/main/java/org/geyser/extension/exampleid/ExampleExtension.java
+++ src/main/java/org/geyser/extension/exampleid/ExampleExtension.java
@@ -4,24 +4,26 @@ import org.geysermc.event.subscribe.Subscribe;
 import org.geysermc.geyser.api.event.lifecycle.GeyserLoadResourcePacksEvent;
 import org.geysermc.geyser.api.event.lifecycle.GeyserPostInitializeEvent;
 import org.geysermc.geyser.api.extension.Extension;
-public class ExampleExtension implements Extension {
 
-    // The main class of your extension - must implement extension, and be in the extension.yml file.
-    // see https://github.com/GeyserMC/Geyser/blob/master/api/src/main/java/org/geysermc/geyser/api/extension/Extension.java for more info on available methods
-
-    // example: accessing extension datafolder
-    // Path thisDataFolder = this.dataFolder();
+// The main class of your extension - must implement extension, and be in the extension.yml file.
+// see https://github.com/GeyserMC/Geyser/blob/master/api/src/main/java/org/geysermc/geyser/api/extension/Extension.java for more info on available methods
+public class ExampleExtension implements Extension 
 
     // Registering custom items/blocks, or adding resource packs (and basically all other events that are fired before Geyser initializes fully)
-    // are done in the PreInitializeEvent. Yeet this if you don't need it.
+    // are done in the PreInitializeEvent. See below for an example:
     @Subscribe
     public void onGeyserPreInitializeEvent(GeyserLoadResourcePacksEvent event) {
         logger().info("Loading: " + event.resourcePacks().size() + " resource packs.");
     }
 
-    // You can use this to run anything after Geyser fully initialized and is ready to accept bedrock player connections.
+    // You can use the GeyserPostInitializeEvent to run anything after Geyser fully initialized and is ready to accept bedrock player connections.
     @Subscribe
     public void onPostInitialize(GeyserPostInitializeEvent event) {
+        // example: show that your extension is loading.
         this.logger().info("Loading example extension...");
+        
+        //example: accessing extension data folder
+        Path exampleDataFolder = this.dataFolder();
+        this.logger().info(exampleDataFolder.toString());
     }
 }
